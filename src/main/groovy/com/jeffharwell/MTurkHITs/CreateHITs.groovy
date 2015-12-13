a9e11a521f9f7891d364353f609317a5cbecc025
Jeff Harwell <jharwell@fuller.edu>
2015-12-13 01:36:48 -0800

    It works

--- src/main/groovy/com/jeffharwell/MTurkHITs/CreateHITs.groovy
+++ src/main/groovy/com/jeffharwell/MTurkHITs/CreateHITs.groovy
@@ -4,6 +4,12 @@ import com.amazonaws.mturk.service.axis.RequesterService;
 import com.amazonaws.mturk.service.exception.ServiceException;
 import com.amazonaws.mturk.util.PropertiesClientConfig;
 import com.amazonaws.mturk.requester.HIT;
+import com.amazonaws.mturk.addon.HITQuestion;
+import com.amazonaws.mturk.requester.Comparator;
+import com.amazonaws.mturk.requester.Locale;
+import com.amazonaws.mturk.requester.QualificationRequirement;
+//import org.apache.log4j.Category;
+//import org.apache.log4j.BasicConfigurator;
 
 class CreateHITs {
 
@@ -23,14 +29,48 @@ class CreateHITs {
         return balance > reward;
     }
 
+
     def createHit() {
+
+        // This is an example of creating a qualification.
+        // This is a built-in qualification -- user must be based in the US
+        QualificationRequirement qualReq = new QualificationRequirement();
+        qualReq.setQualificationTypeId(RequesterService.LOCALE_QUALIFICATION_TYPE_ID);
+        qualReq.setComparator(Comparator.EqualTo);
+        Locale country = new Locale();
+        country.setCountry("US");
+        qualReq.setLocaleValue(country);
+
+        // The create HIT method takes in an array of QualificationRequirements
+        // since a HIT can have multiple qualifications.
+        QualificationRequirement[] qualReqs = [ qualReq ] as QualificationRequirement[]
+        // qualReqs = new QualificationRequirement[] { qualReq };
+
+        HITQuestion question = new HITQuestion("external_question.xml")
+        def prop = new ConfigSlurper().parse(new File("question.properties").toURL())
+        println prop["title"]
         try {
+            HIT hit = service.createHIT(null, // HITTypeId 
+                prop["title"],
+                prop["description"], prop["keywords"],
+                question.getQuestion(),
+                prop["reward"].toDouble(), prop["duration_in_seconds"].toLong(),
+                prop["auto_approval_delay_in_seconds"].toLong(),
+                prop["lifetime_in_seconds"].toLong(),
+                prop["number_of_assignments"].toInteger(),
+                prop["requester_annotation"],
+                qualReqs,
+                null // responseGroup
+            );
+
+            /*
             HIT hit = service.createHIT(
                 title,
                 description,
                 reward,
                 RequesterService.getBasicFreeTextQuestion(
                     "My test question?"),numAssignments);
+            */
 
             println "Created HIT: ${hit.getHITId()}"
 
@@ -42,6 +82,8 @@ class CreateHITs {
     }
 
     public static void main(String[] args) {
+        //BasicConfigurator.configure()
+
         CreateHITs mthit = new CreateHITs()
 
         if (mthit.hasEnoughFund()) {
